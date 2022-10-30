99439eedff2630986b6287045131dbcbc381fdfc
syamagay2 <thundergaya@gmail.com>
2022-10-30 14:25:46 +0900

    add : gitignore

--- bin/test.py
+++ bin/test.py
@@ -1,1 +1,18 @@
 print("Hello World")
+
+import sys,os
+sys.path.append(os.getcwd())
+
+from src import Game
+
+thisgame = Game.Game()
+thissituation = thisgame.send_runner(0,2)
+print(thisgame.current_situation)
+
+thissituation = thisgame.send_runner(2,2)
+thissituation = thisgame.send_runner(0,2)
+print(thisgame.current_situation)
+
+thissituation = thisgame.End_ining()
+print(thisgame.current_situation)
+print(thisgame.game_status)
